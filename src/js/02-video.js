7a8d263a282375f96f73a2ef717c58d122a1f474
IvanAnufriiev <115418712+IvanAnufriiev@users.noreply.github.com>
2023-05-15 13:58:41 +0200

    15.05

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,26 +1,24 @@
-import player from '@vimeo/player';
+import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 const SAVED_TIME = 'videoplayer-current-time';
 const iframe = document.querySelector('iframe');
-const player = new player(iframe);
+const player = new Player(iframe);
 
-player.on('timeupdate', throttle(function ({time}) {
-    localStorage.setItem(SAVED_TIME, time)
+player.on('timeupdate', throttle(function ({seconds}) {
+    localStorage.setItem(SAVED_TIME, seconds)
 }, 1000));
 
 const saveTime = localStorage.getItem(SAVED_TIME);
 const timeStop = JSON.parse(saveTime);
 
-// player.setCurrentTime(localStorage.getItem(CURRENT_TIME) || 0);
 
-player.setCurrentTime(timeStop.time).then(function(time) {}).catch(function(error) {
+player.setCurrentTime(timeStop.seconds).then(function(seconds) {}).catch(function(error) {
     switch (error.name) {
         case 'RangeError':
-            // the time was less than 0 or greater than the video’s duration
             break;
 
         default:
-            // some other error occurred
             break;
     }
 });
+player.setCurrentTime(localStorage.getItem(SAVED_TIME) || 0);
